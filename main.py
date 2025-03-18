847532cc0c165113ab67c552808e244e48951d92
AnuragShre <aquaflames2005@gmail.com>
2025-03-18 16:26:26 +0530

    edit works

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Task:
     def __init__(self,description,status): #initializing
         Task.number_of_task += 1
         self.description = description
-        self.id = Task.number_of_task
+        self.id = str(Task.number_of_task)
         self.status = status
         self.created_at = str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M %Y"))
         self.modified_at = self.created_at
@@ -46,13 +46,14 @@ def welcome():
 def main():
 
 
-    print("Press (1) to add a task\nPress (2) to edit it\nPress (3) to delete it")
+    print("\nPress (1) to add a task\nPress (2) to edit it\nPress (3) to delete it\n")
     choice = input("(1/2/3): ")
     if choice == "1":
         add_newtask()
         main()
     elif choice == "2":
-        pass
+        edit_task()
+        main()
     elif choice=="3":
         pass
     else:
@@ -64,12 +65,46 @@ def main():
 def add_newtask():
     
     tasks.append(new_task())
-    for i in range(len(tasks)):
+    choice = input("Would you like to view all tasks(y/n) :")
+    if choice == "y" or choice == "Y":
+        list_tasks()
+    else:
+        main()
+   
+def edit_task():
+    choice = input("what would you like to edit?\nDescription(d)\nstatus(s)\n: ").lower()    
+    if choice == "d" or choice == "s":
+        print("List of all available tasks: ")
+        list_tasks()
+        if choice == "d":
+            task_id = input("Enter task ID: ")
+            for i in range(len(tasks)):
+        
+                if tasks[i].id == task_id:
+                    new_desc = input("Enter new description: ")
+                    tasks[i].description = new_desc
+        elif choice == "s":
+            task_id = input("Enter task ID: ")
+            for i in range(len(tasks)):
+    
+                if tasks[i].id == task_id:
+                    new_s = input("Set Status (1 = ongoing ; 2 = Done): ")
+                    if new_s == "1":
+                        tasks[i].status = "Ongoing..."
+                    else:
+                        tasks[i].status = "Done."
+    else:
+        print("Please enter a valid choice")
+        edit_task()                
+
+
+
+
+def list_tasks():
+ for i in range(len(tasks)):
         print("**************")
         print(f"* Task ID: {tasks[i].id} *")
         view_task(tasks[i])
-    
-    
     
 
 
